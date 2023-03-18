c4f52dc950f6f3c5c643ae0c5a6601e57d1fb547
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2023-03-18 14:32:13 -0300

    Add files via upload
    Este código Java define uma classe chamada "Main". Dentro da classe, há um método chamado "main" que é o ponto de entrada para o programa. O método "main" recebe um argumento do tipo String[] chamado "args".
    
    Dentro do método "main", duas variáveis do tipo inteiro são declaradas e inicializadas. A primeira variável é chamada "intA" e é inicializada com o valor 1. A segunda variável é chamada "intB" e é inicializada com o valor da primeira variável "intA".
    
    Em seguida, o programa imprime uma mensagem na tela usando o método "println" da classe "System". A mensagem exibe os valores das duas variáveis "intA" e "intB" concatenados em uma única string.
    
    No final, o programa termina sua execução.

---
+++ EstruturaDeDadosJava/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        
+        
+        int intA = 1;
+        int intB = intA;
+        
+        
+        System.out.println("intA=" +intA+"intB="+ intB);
+        intA = 2;
+         System.out.println("intA=" +intA+"intB="+ intB);
+    }
+}
